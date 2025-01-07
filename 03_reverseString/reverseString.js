5923ffd1167833cce60d464d84d5059ec0f93231
Felivander <feli.vander@gmail.com>
2025-01-07 14:41:18 +0000

    finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,7 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let stringArray = str.split('');
+    let reversedString = stringArray.reverse().join('');
+    return reversedString;
 
 };
 
