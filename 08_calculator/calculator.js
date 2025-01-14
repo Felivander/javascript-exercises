6d8dc89a00b31c6e3b2d38d136541ea8744c9e06
Felivander <feli.vander@gmail.com>
2025-01-14 20:04:37 +0000

    finished js array exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+	arr.forEach(element => {
+    sum += element;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let multplication = 1;
+  arr.forEach(element => {
+    multplication *= element;
+  })
+  return multplication;
 };
 
-const power = function() {
-	
+const power = function(num, exponent) {
+	let power = 1;
+  for (let i = 0; i < exponent; i++) {
+    power = power * num;
+  }
+  return power;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let factorial = 1;
+	for (let i = num; i > 0 ; i--) {
+    factorial *= i;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
